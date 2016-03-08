bdbee16deba802e49c9ef3e95f7f44e1a44c1199
Greg Restall <greg@consequently.org>
2016-03-09 09:11:45 +1100

    Entry for paper for On-Priest-on-Nonmonotonic

---
+++ public/im2004/mtview.php
@@ -0,0 +1,5 @@
+<?php
+    include('/usr/home/0chroot/consequ/home/consequ/public_html/mt/php/mt.php');
+    $mt = new MT(5, '/usr/home/0chroot/consequ/home/consequ/public_html/mt/mt.cfg');
+    $mt->view();
+?>
