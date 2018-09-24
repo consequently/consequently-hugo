93b44ad25624baa501473f00c59a85ea153d4e5d
greg@consequently.org <greg@consequently.org>
2018-09-24 00:36:58 +0000

    Update from Forestry.io - Updated Forestry configuration

--- public/im2004/mtview.php
+++
@@ -1,5 +0,0 @@
-<?php
-    include('/usr/home/0chroot/consequ/home/consequ/public_html/mt/php/mt.php');
-    $mt = new MT(5, '/usr/home/0chroot/consequ/home/consequ/public_html/mt/mt.cfg');
-    $mt->view();
-?>
